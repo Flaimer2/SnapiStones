cde7afa32e128604d741ca03b333be06e84454fc
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2023-05-29 18:52:19 +0300

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,8 +73,7 @@ tasks {
 
     compileJava {
         options.encoding = Charsets.UTF_8.name()
-        options.release.set(11)
-        options.compilerArgs.add("-parameters")
+        options.compilerArgs.addAll(listOf("-nowarn", "-Xlint:-unchecked", "-Xlint:-deprecation"))
     }
 
     processResources {
